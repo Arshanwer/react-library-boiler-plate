889521fec74bace3e822b266a4672f886e3fdf33
Arshad Anwer <arshad_91@live.com>
2019-09-17 17:08:44 +0800

    init example

---
+++ example2/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { NavBar } from '@arshanwer/boiler-plate';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <NavBar>asss</NavBar>
+    </div>
+  );
+}
+
+export default App;
